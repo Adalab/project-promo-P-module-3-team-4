0e6b907b2a043c7c6311cd6d2c5c4bef86dbacd3
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-04-28 13:36:20 +0200

    components and layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,11 @@
 import '../styles/App.scss';
-// import { useState } from 'react';
 import logo from '../images/logo-adalab.png';
-import team from '../images/logo-team.png';
 
 const Footer = () => {
   return (
     <footer>
       <div className="footer">
-        <img className="footer__img" src={team} alt="" />
+        <img className="footer__img" src={logo} alt="" />
         <h2 className="h2">Awesome profile-cards @2018 Equipo 6</h2>
 
         <a title="adalab" href="https://www.adalab.es/">
